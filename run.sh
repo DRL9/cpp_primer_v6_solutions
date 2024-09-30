20847466c1b27d3c7f868522bc70bbf89226b511
Yuanjun Zhong <937676516@qq.com>
2024-09-30 16:08:43 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-find src -name *.cpp -type f | sort | awk -F / '{print "add_executable("$2 "_" $3 " " $0")"}' | pbcopy
+cmakefile="./CMakeLists.txt"
+# 删除原来的行
+sed -i '' '/add_executable.*/d' $cmakefile
+# 添加全量的行
+find src -name *.cpp -type f | sort | awk -F / '{print "add_executable("$2 "_" $3 " " $0")"}' >>$cmakefile
