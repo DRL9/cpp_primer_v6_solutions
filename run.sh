049bbf9648e79d7e84eaba2032f47e3011005456
Yuanjun Zhong <937676516@qq.com>
2024-09-30 09:22:06 +0800

    add sol_2_3

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-find src -name *.cpp -type f | awk -F / '{print "add_executable("$2 "_" $3 " " $0")"}'
+find src -name *.cpp -type f | sort | awk -F / '{print "add_executable("$2 "_" $3 " " $0")"}' | pbcopy
