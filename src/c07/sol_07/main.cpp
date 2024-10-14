1640d3ce13ff779740b87b27a3f22f41ca1077a6
Yuanjun Zhong <937676516@qq.com>
2024-10-14 09:55:51 +0800

    add c07_7,8

--- src/c07/sol_07/main.cpp
+++ src/c07/sol_07/main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int fill_array(double arr[], int limit) {
+double* fill_array(double arr[], int limit) {
   int i;
+  double temp;
   for (i = 0; i < limit; i++) {
+    cout << "Enter value #" << (i + 1) << ":";
+    if (!(cin >> temp)) {
+      cin.clear();
+      while (cin.get() != '\n')
+        continue;
+      cout << "input process terminated" << endl;
+      break;
+    } else if (temp < 0) {
+      break;
+    }
+    *arr = temp;
+    arr++;
   }
-  return i;
+  return arr;
+}
+
+void show_array(const double arr[], const double* end) {
+  int i = 0;
+  while (arr != end) {
+    cout << "#" << (i + 1) << ":" << *arr << endl;
+    i++;
+    arr++;
+  }
 }
 
 int main() {
+  const int MAX = 10;
+  double arr[MAX];
+  auto end = fill_array(arr, MAX);
+  show_array(arr, end);
   return 0;
 }
--- src/c07/sol_08/main.cpp
+++ src/c07/sol_08/main.cpp
@@ -1,8 +1,36 @@
+// 方案b
+#include <array>
 #include <iostream>
+using namespace std;
+
+const int Seasons = 4;
+const char* Snames[Seasons] = {"Spring", "Summer", "Fall", "Winter"};
+
+struct Expense {
+  double value[Seasons];
+};
+
+void fill(Expense* pa);
+void show(Expense da);
 
 int main() {
-  using namespace std;
-  cout << "name: DRL9" << endl;
-  cout << "address: China" << endl;
+  Expense expenses;
+  fill(&expenses);
+  show(expenses);
   return 0;
+}
+void fill(Expense* pa) {
+  for (int i = 0; i < Seasons; i++) {
+    std::cout << "Enter " << Snames[i] << " expenses: ";
+    std::cin >> (pa->value)[i];
+  }
+}
+void show(Expense da) {
+  double total = 0.0;
+  std::cout << "\nEXPENSES\n";
+  for (int i = 0; i < Seasons; i++) {
+    std::cout << Snames[i] << ": $" << da.value[i] << std::endl;
+    total += da.value[i];
+  }
+  std::cout << "Total Expenses: $" << total << std::endl;
 }
